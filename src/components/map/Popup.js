0017b734cdf0af7b26604a04cdd47d91e2ff30d5
chuangeng555 <psp.chuangeng@gmail.com>
2020-12-09 23:02:15 +0800

    changes

--- src/components/map/Popup.js
+++ src/components/map/Popup.js
@@ -1,13 +1,22 @@
 import React, { Component, useState, useEffect } from 'react';
-
+import Button from '@material-ui/core/Button';
 
 export default function Popup() {
+    const [submission, setSubmission] = useState(0);
 
     return (
         <div className='popup'>
-            Hello
+            <Button id = "close" variant="contained" color="secondary" type="submit" onClick={() => setSubmission(submission + 1)}>X</Button>
+            <form className="form"> 
+                    Title : {" "} 
+                    <input type="text" name="title"/>
+                    Location : {" "} 
+                    <input type="text" name="location"/>
+                    Description : {" "}
+                    <textarea id="description"></textarea>
+                    <Button variant="contained" color="primary" type="submit" onClick={() => setSubmission(submission + 1)}>Submit</Button>
+            </form>
         </div>
 
     )
-
 }
--- src/components/map/Usermap.js
+++ src/components/map/Usermap.js
@@ -15,7 +15,48 @@ class Usermap extends Component {
             currentLocation: { lat: 1.3521, lng: 103.8198 },
             zoom: 12,
             showPopup: false,
+            showSearch: false,
+            searchTerm: " ",
         }
+    }
+
+
+    onSearchSubmit = () => {
+        const { searchTerm } = this.state;
+        this.fetchSearchTopLocation(searchTerm);
+      }
+
+    onSearchChange = (event) => {
+        console.log(event.target.value);
+        this.setState({ searchTerm: event.target.value });
+      }
+
+    fetchSearchTopLocation = (searchTerm) => {
+
+    } 
+
+    _onCreated = (e) => {
+        console.log(e)
+        //pass geo json here 
+
+
+        this.setState({
+            showPopup: true,
+        })
+    }
+
+    togglePopupClose = () => {  
+        this.setState({  //to set state 
+             showPopup: false  //Opposite of default if click --> showPopup : True  
+        });  
+    }
+
+    togglePopupOpen = () => {
+        this.setState({showPopup: true})
+    }
+
+    toggleSearchOpen = () => {
+        this.setState({showSearch: true})
     }
 
 
@@ -23,33 +64,30 @@ class Usermap extends Component {
         
     }
 
-    _onCreated = (e) => {
-        console.log(e)
-        this.setState({
-            showPopup: true,
-        })
-    } 
-
-    togglePopup = () => {  
-        this.setState({  //to set state 
-             showPopup: false  //Opposite of default if click --> showPopup : True  
-        });  
-    }
-
-
     render () {
-
-
-        const {currentLocation, zoom } = this.state;
+        
+        const {currentLocation, zoom, searchTerm} = this.state;
         return (
         <div>
-            <Map className="map" center={currentLocation} zoom={zoom}>
-            {this.state.showPopup ? <Popup closePopup={this.togglePopup}  />  : null }
+            <Map className="map" center={currentLocation} zoom={zoom}> 
+            <button className="searchBar" onClick={this.toggleSearchOpen}> Search </button>
+            
+            {this.state.showSearch ?
+            <Search
+            className = "searchBar"
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
+            ></Search>
+            : null }
+
+            {this.state.showPopup ?
+             <Popup closePopup={this.togglePopupClose}  /> 
+              : null }
             <TileLayer
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 					attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
 					/>
-                    
                     <FeatureGroup>
 						<EditControl
 						position='topleft'
@@ -73,6 +111,16 @@ class Usermap extends Component {
 
     }
 }
+
+
+const Search = ({ value, onChange }) => (
+    <div className="popup"> 
+    <form>
+      Serach {" "}
+      <input type="text" value={value} onChange={onChange} />
+    </form>
+    </div>
+  );
 
 
 export default Usermap;
